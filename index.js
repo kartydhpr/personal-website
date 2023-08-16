07898a5576c3aba211cc11bf22ff247ca75dc517
karteikaydhuper <karteikaydhuper@gmail.com>
2023-08-15 23:37:47 -0400

    remove picture on hover effect

--- index.js
+++ index.js
@@ -141,3 +141,19 @@ function scrollProgress() {
   scrollProgressBar.style.width = percentageScrolled + "%";
 }
 document.addEventListener("scroll", scrollProgress);
+
+
+// $(document).on("scroll", function() {
+//   var pageTop = $(document).scrollTop();
+//   var pageBottom = pageTop + $(window).height();
+//   var tags = $(".tag");
+
+//   for (var i = 0; i < tags.length; i++) {
+//     var tag = tags[i];
+//     if ($(tag).position().top < pageBottom) {
+//       $(tag).addClass("visible");
+//     } else {
+//       $(tag).removeClass("visible");
+//     }
+//   }
+// });
